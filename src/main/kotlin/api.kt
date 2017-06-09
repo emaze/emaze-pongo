e3f197ecbe5c52a89ac29ac72eb964691ca55ebe
Mauro Rocchi <mauro.rocchi@emaze.net>
2017-06-09 18:45:23 +0200

    dev: write some docs

--- src/main/kotlin/api.kt
+++ src/main/kotlin/api.kt
@@ -1,13 +1,21 @@
 package net.emaze.pongo
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import java.lang.IllegalArgumentException
 import java.lang.RuntimeException
 import java.util.*
 
+/**
+ * Identifiable is the base class of an entity.
+ * It maintains the metadata (surrogate ID and version number) needed to identify the entity in the store.
+ */
 abstract class Identifiable(@JsonIgnore var metadata: Metadata? = null) {
 
     data class Metadata(val identity: Long, val version: Long)
 
+    /**
+     * Two Identifiables are considered equals if they have the same class and the same metadata.
+     */
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other === null) return false
@@ -35,23 +43,54 @@ class OptimisticLockException(message: String) : RuntimeException(message)
 
 interface EntityRepository<T : Identifiable> {
 
-    @Throws(OptimisticLockException::class)
+    /**
+     * Save or update the entity.
+     *
+     * @throws OptimisticLockException if the entity has been already updated by another transaction
+     */
     fun save(entity: T): T
 
+    /**
+     * Delete the entity.
+     *
+     * @throws IllegalArgumentException if the given entity has not an identity
+     * @throws IllegalStateException if the entity doesn't exists of the store
+     */
     fun delete(entity: T)
 
+    /**
+     * Delete all the entities without lock the whole repository.
+     */
     fun deleteAll()
 
+    /**
+     * Get all the entities.
+     */
     fun findAll(): List<T> = findAll()
 
+    /**
+     * Get all the entities satisfying the query.
+     */
     fun findAll(query: String, vararg params: Any): List<T>
 
+    /**
+     * Get all the entities with the properties equals to the example's ones.
+     */
     fun findAllLike(example: Any): List<T>
 
+    /**
+     * Get the first entity.
+     */
     fun findFirst(): Optional<T> = findFirst("")
 
+    /**
+     * Get the first entity satisfying the query.
+     */
     fun findFirst(query: String, vararg params: Any): Optional<T>
 
+    /**
+     * Get the first entity with the properties equals to the example's ones.
+     */
     fun findFirstLike(example: Any): Optional<T>
 }
 
