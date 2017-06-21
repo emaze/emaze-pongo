88888ed4ebcd775007e5bb70c991a3c2e8270fa1
Mauro Rocchi <mauro.rocchi@emaze.net>
2017-06-21 10:01:22 +0200

    ref: cleanups

--- src/main/kotlin/annotation.kt
+++ src/main/kotlin/annotation.kt
@@ -19,42 +19,51 @@ annotation class Query(val value: String)
 annotation class Nullable
 
 fun <T : Identifiable, R : EntityRepository<T>> EntityRepository<T>.create(targetClass: Class<R>): R {
-    fun invoke(method: Method): Handler<T> = { repository, args -> method.invoke(repository, *args) }
     val repository = this
-    val handlers = mutableMapOf<Method, Handler<T>>()
+    val handlers = mutableMapOf<Method, Handler>()
     @Suppress("UNCHECKED_CAST")
     val proxy = Proxy.newProxyInstance(javaClass.classLoader, arrayOf(targetClass), { _, method, args ->
-        val handler = handlers[method] ?: throw UnsupportedOperationException("Unsupported method ${method.returnType}")
-        handler(repository, args ?: emptyArray())
+        val handler = handlers[method] ?: throw UnsupportedOperationException("Unsupported method $method")
+        handler(args ?: emptyArray())
     }) as R
     val baseMethods: List<Method> = listOf(*EntityRepository::class.java.declaredMethods)
     baseMethods.associateByTo(
         handlers,
         { method -> method },
-        { method -> invoke(method) }
+        { method -> { args -> method.invoke(repository, *args) } }
     )
     val userMethods = listOf(*targetClass.methods) - baseMethods
     userMethods.associateByTo(
         handlers,
         { method -> method },
-        { method -> getHandler(repository, method, targetClass, proxy) }
+        { method ->
+            if (method.isDefault) invokeDefaultMethod(method, targetClass, proxy)
+            else makeHandler(method, repository)
+        }
     )
     return proxy
 }
 
-private typealias Handler<T> = (EntityRepository<T>, Array<Any?>) -> Any?
+private typealias Handler = (Array<Any?>) -> Any?
 
-private fun <T : Identifiable> getHandler(repository: EntityRepository<T>, method: Method, targetClass: Class<*>, proxy: Any): Handler<T> {
-    fun findAllAsList(query: String): Handler<T> = { repository, args -> repository.findAll(query, *args) }
-    fun findAllAsSet(query: String): Handler<T> = { repository, args -> LinkedHashSet(repository.findAll(query, *args)) }
-    fun findFirstOptional(query: String): Handler<T> = { repository, args -> repository.findFirst(query, *args) }
-    fun findFirstNullable(query: String): Handler<T> = { repository, args -> repository.findFirst(query, *args).orElse(null) }
-    fun findFirstNotNull(query: String): Handler<T> = { repository, args ->
+private fun invokeDefaultMethod(method: Method, targetClass: Class<*>, proxy: Any): Handler {
+    val handler = MethodHandles.Lookup::class.java.getDeclaredConstructor(Class::class.java, Int::class.javaPrimitiveType)
+        .apply { isAccessible = true }
+        .newInstance(targetClass, -1 /* trusted */)
+        .unreflectSpecial(method, targetClass)
+        .bindTo(proxy)
+    return { args -> handler.invokeWithArguments(*args) }
+}
+
+private fun <T : Identifiable> makeHandler(method: Method, repository: EntityRepository<T>): Handler {
+    fun findAllAsList(query: String): Handler = { args -> repository.findAll(query, *args) }
+    fun findAllAsSet(query: String): Handler = { args -> LinkedHashSet(repository.findAll(query, *args)) }
+    fun findFirstOptional(query: String): Handler = { args -> repository.findFirst(query, *args) }
+    fun findFirstNullable(query: String): Handler = { args -> repository.findFirst(query, *args).orElse(null) }
+    fun findFirstNotNull(query: String): Handler = { args ->
         repository.findFirst(query, *args).orElseThrow { NoSuchElementException("The query $query has no result") }
     }
-    if (method.isDefault) {
-        return invokeDefaultMethod(method, targetClass, proxy)
-    }
+
     val query = method.getAnnotation(Query::class.java)?.value ?: throw UnsupportedOperationException("The method ${method.name} has no @Query annotation")
     val nullable = method.isAnnotationPresent(Nullable::class.java)
     return when {
@@ -65,14 +74,3 @@ private fun <T : Identifiable> getHandler(repository: EntityRepository<T>, metho
         else -> throw UnsupportedOperationException("Unsupported return type ${method.returnType} of method ${method.name}")
     }
 }
-
-private fun <T : Identifiable> invokeDefaultMethod(method: Method, targetClass: Class<*>, proxy: Any): Handler<T> {
-    val handler = MethodHandles.Lookup::class.java.getDeclaredConstructor(Class::class.java, Int::class.javaPrimitiveType)
-        .apply { isAccessible = true }
-        .newInstance(targetClass, -1 /* trusted */)
-        .unreflectSpecial(method, targetClass)
-        .bindTo(proxy)
-    return { _, args -> handler.invokeWithArguments(*args) }
-}
-
-
