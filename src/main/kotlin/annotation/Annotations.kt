dab1884ecf89639453a0f743c9d8dc577548bb87
Mauro Rocchi <mauro.rocchi@emaze.net>
2019-01-08 10:04:48 +0100

    documentation

--- src/main/kotlin/annotation/Annotations.kt
+++ src/main/kotlin/annotation/Annotations.kt
@@ -1,9 +1,15 @@
 package net.emaze.pongo.annotation
 
+/**
+ * Declares the "where" clause used by the annotated repository method.
+ */
 @Target(AnnotationTarget.FUNCTION)
 @MustBeDocumented
 annotation class Where(val value: String)
 
+/**
+ * Indicates that the annotated repository method can return null.
+ */
 @Target(AnnotationTarget.FUNCTION)
 @MustBeDocumented
 annotation class Nullable
